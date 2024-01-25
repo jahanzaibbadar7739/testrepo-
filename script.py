0b3ca0ed6afe7d057f16b425991d8dda1ca90e97
Jahanzaib Badar <jahanzaibsain@outlook.com>
2024-01-25 15:04:15 +0500

    Add files via upload

---
+++ script.py
@@ -0,0 +1,17 @@
+print("hello world") #this will print a string
+print("Jahanzaib")
+#print("abro")
+
+#ctrl+/(to comment or un comment)
+
+# this line will print a number with + function
+print(3+7)
+
+#variables
+
+x = 3+6*4
+print(x)
+print(x+4)
+print(x*3)
+print(x-5)
+import tensorflow as tf 
